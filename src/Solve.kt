bc8b9baf819bdb4276e51cd159700a11a0a32db0
Adam Hurwitz <3938076+AdamSHurwitz@users.noreply.github.com>
2020-11-11 12:12:47 -0800

    init: Kotlin Data Structure Template

--- src/Solve.kt
+++ src/Solve.kt
@@ -1,1 +1,38 @@
-// Todo: Run logic in test class.
+import java.util.*
+
+fun someFun(){
+    val someString = "Some string"
+
+    val someInt = 18
+
+    val someDouble = 18.18
+
+    val someBoxedArray = arrayOf("stringOne", "stringTwo", "stringThree")
+
+    // More performant using primitive type vs. object
+    val someUnboxedArray = charArrayOf('c')
+
+    val someUnboxedNullArray = CharArray(18)
+
+    val someTwoDArray = arrayOf(arrayOf("stringOne"), arrayOf("stringTwo"), arrayOf("stringThree"))
+
+    val someArrayList = arrayListOf("stringOne", "stringTwo", "stringThree")
+
+    val someLinkedList = LinkedList<SomeObject>()
+
+    // Use for stacks and queues.
+    val someArrayDeque = ArrayDeque<String>()
+
+    // Use for min/max heap
+    val somePriorityQueue = PriorityQueue<String>()
+
+    val someHashMap = hashMapOf<String, Int>()
+
+    // Use for binary search.
+    val someTreeMap = TreeMap<String, Int>()
+
+    val someHashSet = hashSetOf<String>()
+
+}
+
+class SomeObject(val someData: String)
--- src/test/TestSolve.kt
+++ src/test/TestSolve.kt
@@ -2,9 +2,6 @@ package test
 
 import org.assertj.core.api.Assertions.assertThat
 import org.junit.jupiter.api.MethodOrderer
-import org.junit.jupiter.api.Nested
-import org.junit.jupiter.api.Order
-import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.TestInstance
 import org.junit.jupiter.api.TestMethodOrder
 import org.junit.jupiter.params.ParameterizedTest
@@ -28,26 +25,4 @@ class TestSolve {
         assertThat(test.input).isEqualTo(test.expect)
     }
 
-    @Test
-    @Order(1)
-    fun `First test`() {
-        printAssert(18, 18)
-        assertThat(18).isEqualTo(18)
-    }
-
-    @Nested
-    inner class `First nested test` {
-        @Test
-        @Order(2)
-        fun `First inner test`() {
-            printAssert(18, 18)
-            assertThat(18).isEqualTo(18)
-        }
-        @Test
-        @Order(3)
-        fun `Second inner test`() {
-            printAssert(18, 18)
-            assertThat(18).isEqualTo(18)
-        }
-    }
 }
